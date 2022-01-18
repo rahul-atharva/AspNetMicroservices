d0e373d0d699bdac6d73f82cd48af9c8a23fc77b
Rahul Patel <rpatel@brightree.com>
2022-01-18 16:07:25 +0530

    Adding DIscount.Grpc services in Basket.API and added Discount.Grpc to docker

---
+++ src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcServices.cs
@@ -0,0 +1,23 @@
+﻿using Discount.Grpc.Protos;
+using System;
+using System.Threading.Tasks;
+
+namespace Basket.API.GrpcServices
+{
+    public class DiscountGrpcServices
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoService;
+
+        public DiscountGrpcServices(DiscountProtoService.DiscountProtoServiceClient discountProtoServices)
+        {
+            _discountProtoService = discountProtoServices ?? throw new ArgumentNullException(nameof(discountProtoServices));
+        }
+
+        public async Task<CouponModel> GetDiscount(string productName)
+        {
+            var discountRequest = new GetDiscountRequest { ProductName = productName };
+
+            return await _discountProtoService.GetDiscountAsync(discountRequest);
+        }
+    }
+}
--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -1,5 +1,7 @@
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Basket.API.Repositories.Interfaces;
+using Discount.Grpc.Protos;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -35,6 +37,11 @@ namespace Basket.API
 
             // General Configuration
             services.AddScoped<IBasketRepository, BasketRepository>();
+
+            services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
+                (o => o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
+
+            services.AddScoped<DiscountGrpcServices>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
